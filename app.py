658bdf25e3669f9fb6d3e831bfc7f5a840af8503
Guido <guido-visser@hotmail.com>
2022-03-18 22:29:12 +0100

    added toggles for true positions and export button

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import numpy as np
+import cv2
 
 from constants import MAP_WIDTH, MAP_HEIGHT
 from src.position import Position
 from src.generate_map import Map
 from src.icons import Icon, ICONS
 
-PLAYER_ICON = Icon(Position(0, 0), lambda x: 0, lambda x: 0, img_path="images/icons/aluminium.png")
+LAST_POSITION = Position(0,0)
+PLAYER_ICON = Icon("player", Position(0, 0), lambda x: 0, lambda x: 0, img_path="images/icons/aluminium.png")
 
 def move(event):
     global bg_img # because tkinter
@@ -22,12 +24,81 @@ def move(event):
     mouse_icon = ImageTk.PhotoImage(image=pil_icon)
     canvas.create_image(event.x, event.y, image=mouse_icon)
 
+    global LAST_POSITION
+    LAST_POSITION = Position(event.x, event.y, mode="absolute")
+
+class IconButton(object):
+    def __init__(self, button_bar, icon, index) -> None:
+        super().__init__()
+
+        self.button_bar = button_bar
+        self.icon = icon
+        self.index = index
+        self.active = False
+        self.render_button()
+
+    def render_button(self):
+        global icon_image_list
+        if self.active:
+            img = Image.fromarray(self.icon.image)
+        else:
+            img = Image.fromarray(self.icon.image // 2)
+
+        icon_image_list[self.index] = ImageTk.PhotoImage(img)
+        self.button = Button(self.button_bar, image=icon_image_list[self.index], command=self.button_press)
+        self.button.grid(row=self.index, column=0)
+    
+        self.text = Text(self.button_bar, height=2, width=8)
+        self.text.insert(INSERT, self.icon.name)
+        self.text.grid(row=self.index, column=1)
+
+    def button_press(self):
+        self.icon.show_true_position = not self.icon.show_true_position
+        self.active = not self.active
+        self.render_button()
+        move(LAST_POSITION)
+
+
+class ExportButton(object):
+    def __init__(self, button_bar) -> None:
+        super().__init__()
+
+        self.button_bar = button_bar
+        self.render_button()
+        
+    def render_button(self):
+        global export_img
+        img = Image.open("images/export.png")
+        img = img.resize((40, 40))
+        export_img = ImageTk.PhotoImage(img)
+        self.button = Button(self.button_bar, image=export_img, command=self.export)
+        self.button.grid(row=len(ICONS) + 1, column=0)
+
+        self.text = Text(self.button_bar, height=2, width=8)
+        self.text.insert(INSERT, "Export")
+        self.text.grid(row=len(ICONS) + 1, column=1)
+
+    def export(self):
+        out_img = map.render(LAST_POSITION)
+        out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite("images/out.png", out_img)
+
 root = Tk()
 root.title("Treasure Map")
-root.geometry("788x1000")
 
 canvas = Canvas(root, width=MAP_WIDTH, height=MAP_HEIGHT, bg="white")
-canvas.pack(pady=20)
+icon_bar = Frame(root, borderwidth=2, relief="raised")
+
+canvas.pack(side=LEFT, fill='both', expand=True)
+icon_bar.pack(side=RIGHT, fill='both', expand=True)
+
+icon_image_list = list(np.arange(len(ICONS)))
+icons_buttons = [IconButton(icon_bar, icon, index=i) for i, icon in enumerate(ICONS)]
+
+export_button = ExportButton(icon_bar)
+
+icon_bar.grid_rowconfigure(100, weight=1)
+icon_bar.grid_columnconfigure(2, weight=1)
 
 map_path = "images/dessarin_valley.jpg"
 
--- src/icons.py
+++ src/icons.py
@@ -9,93 +9,99 @@ TREASURE_POSITION = Position(0.6558, 0.5755)
 
 class Icon(object):
     def __init__(self, 
+                 name,
                  true_position: Position, 
                  radius_func: object, 
                  angle_func: object,
                  img_path: str = "images/icons/brass.png",
                  img_offset: Position = Position(-0.02, -0.02),
                  show_true_position: bool = False) -> None:
-        super().__init__()
-       
-        self.true_position = true_position + img_offset
-        self.show_true_position = show_true_position
-        self.radius_func = radius_func
-        self.angle_func = angle_func
+     super().__init__()
 
-        self.image = cv2.resize(cv2.imread(img_path, cv2.IMREAD_UNCHANGED), (int(0.04 * MAP_HEIGHT), int(0.04 * MAP_HEIGHT)))
-        self.alpha = np.stack([(cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY) <= 128).astype(np.uint8)]*3, axis=2)
+     self.name = name
+     self.true_position = true_position + img_offset
+     self.show_true_position = show_true_position
+     self.radius_func = radius_func
+     self.angle_func = angle_func
 
-        self.alpha = cv2.dilate(self.alpha, kernel=np.ones((3,3), dtype=np.uint8), iterations=2)
+     self.image = cv2.resize(cv2.imread(img_path, cv2.IMREAD_UNCHANGED), (int(0.04 * MAP_HEIGHT), int(0.04 * MAP_HEIGHT)))
+     self.alpha = np.stack([(cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY) <= 128).astype(np.uint8)]*3, axis=2)
 
-        self.size = self.image.shape[0:2]
-        
-        center_x = true_position.x - radius_func(TREASURE_POSITION) * cos(angle_func(TREASURE_POSITION))
-        center_y = true_position.y - radius_func(TREASURE_POSITION) * sin(angle_func(TREASURE_POSITION))
-        self.center_position = Position(center_x, center_y, mode="absolute") + img_offset
+     self.alpha = cv2.dilate(self.alpha, kernel=np.ones((3,3), dtype=np.uint8), iterations=2)
+
+     self.size = self.image.shape[0:2]
+     
+     center_x = true_position.x - radius_func(TREASURE_POSITION) * cos(angle_func(TREASURE_POSITION))
+     center_y = true_position.y - radius_func(TREASURE_POSITION) * sin(angle_func(TREASURE_POSITION))
+     self.center_position = Position(center_x, center_y, mode="absolute") + img_offset
 
     def location(self, position: Position):
         
-        radius = self.radius_func(position)
-        angle  = self.angle_func(position)
+          radius = self.radius_func(position)
+          angle  = self.angle_func(position)
 
-        u = int(radius * cos(angle) + self.center_position.x)
-        v = int(radius * sin(angle) + self.center_position.y)
+          u = int(radius * cos(angle) + self.center_position.x)
+          v = int(radius * sin(angle) + self.center_position.y)
 
-        return (u, v)
+          return (u, v)
 
 ICONS = [
-        Icon(Position(0.5, 0.2),
-             lambda pos: MAP_WIDTH * 0.1,# (0.1 + 0.1 * cos(6 * (2 * pos.x + pos.y) / MAP_WIDTH)),
-             lambda pos: 3 * (2 * pos.x + pos.y) / MAP_WIDTH,
-             img_path = "images/icons/brass.png",
-             show_true_position=True),
-        Icon(Position(0.65, 0.35),
-             lambda pos: MAP_WIDTH * 0.05,# * (0.05 + 0.1 * cos(pos.x / 10)),
-             lambda pos: -7 * (pos.x + 2 * pos.y) / MAP_WIDTH + 2,
-             img_path = "images/icons/iron.png",
-             show_true_position=True),
-        Icon(Position(0.45, 0.55),
-             lambda pos: MAP_WIDTH * 0.22,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: 9 * (pos.x + pos.y) / MAP_WIDTH + 2.5,
-             img_path = "images/icons/zinc.png",
-             img_offset=Position(-0.03014, -0.00125),
-             show_true_position=True),
-        Icon(Position(.5105, .605),
-             lambda pos: MAP_WIDTH * 0.13,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: -11 * (pos.x * 3 + pos.y) / MAP_WIDTH + 3.5,
-             img_path = "images/icons/tin.png",
-             img_offset=Position(-.02538, -0.02625)),
-        Icon(Position(0.4892, 0.64075),
-             lambda pos: MAP_WIDTH * 0.3,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: 9 * (pos.y) / MAP_WIDTH + .5,
-             img_path = "images/icons/tin.png",
-             img_offset=Position(-0.02538, -0.02625)),
-        Icon(Position(0.3972, 0.627),
-             lambda pos: MAP_WIDTH * 0.17,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: -3 * (pos.x + pos.y * 4) / MAP_WIDTH + 1.5,
-             img_path = "images/icons/tin.png",
-             img_offset=Position(-0.02538, -0.02625)),
-        Icon(Position(0.4546, 0.60175),
-             lambda pos: MAP_WIDTH * 0.2,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: 9 * (pos.x + pos.y) / MAP_WIDTH + 5.5,
-             img_path = "images/icons/tin.png",
-             img_offset=Position(-0.02538, -0.02625)),
-        Icon(Position(0.3813, 0.5535),
-             lambda pos: MAP_WIDTH * 0.08,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: -11 * (pos.x) / MAP_WIDTH + .5,
-             img_path = "images/icons/gold.png",
-             img_offset=Position(-0.02538, -0.02),
-             show_true_position=True),
-        Icon(Position(0.5555, 0.54925),
-             lambda pos: MAP_WIDTH * 0.17,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: 5 * (pos.x * 2 + pos.y) / MAP_WIDTH + 3.5,
-             img_path = "images/icons/electrum.png",
-             img_offset=Position(-0.02538, -0.02),
-             show_true_position=True),
-        Icon(Position(0.5359, 0.48725),
-             lambda pos: MAP_WIDTH * 0.15,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
-             lambda pos: -13 * (pos.x + pos.y) / MAP_WIDTH,
-             img_path = "images/icons/cadmium.png",
-             img_offset=Position(-0.02538, -0.02),
-             show_true_position=True)
+     Icon("brass",
+          Position(0.5, 0.2),
+          lambda pos: MAP_WIDTH * 0.1,# (0.1 + 0.1 * cos(6 * (2 * pos.x + pos.y) / MAP_WIDTH)),
+          lambda pos: 3 * (2 * pos.x + pos.y) / MAP_WIDTH,
+          img_path = "images/icons/brass.png"),
+     Icon("iron",
+          Position(0.65, 0.35),
+          lambda pos: MAP_WIDTH * 0.05,# * (0.05 + 0.1 * cos(pos.x / 10)),
+          lambda pos: -7 * (pos.x + 2 * pos.y) / MAP_WIDTH + 2,
+          img_path = "images/icons/iron.png"),
+     Icon("zinc",
+          Position(0.45, 0.55),
+          lambda pos: MAP_WIDTH * 0.22,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: 9 * (pos.x + pos.y) / MAP_WIDTH + 2.5,
+          img_path = "images/icons/zinc.png",
+          img_offset=Position(-0.03014, -0.00125)),
+     Icon("gold",
+          Position(0.3813, 0.5535),
+          lambda pos: MAP_WIDTH * 0.08,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: -11 * (pos.x) / MAP_WIDTH + .5,
+          img_path = "images/icons/gold.png",
+          img_offset=Position(-0.02538, -0.02)),
+     Icon("silver",
+          Position(0.5555, 0.54925),
+          lambda pos: MAP_WIDTH * 0.17,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: 5 * (pos.x * 2 + pos.y) / MAP_WIDTH + 3.5,
+          img_path = "images/icons/electrum.png",
+          img_offset=Position(-0.02538, -0.02)),
+     Icon("cadmium",
+          Position(0.5359, 0.48725),
+          lambda pos: MAP_WIDTH * 0.15,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: -13 * (pos.x + pos.y) / MAP_WIDTH,
+          img_path = "images/icons/cadmium.png",
+          img_offset=Position(-0.02538, -0.02)),
+     Icon("door",
+          Position(.5105, .605),
+          lambda pos: MAP_WIDTH * 0.13,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: -11 * (pos.x * 3 + pos.y) / MAP_WIDTH + 3.5,
+          img_path = "images/icons/tin.png",
+          img_offset=Position(-.02538, -0.02625)),
+     Icon("door",
+          Position(0.4892, 0.64075),
+          lambda pos: MAP_WIDTH * 0.3,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: 9 * (pos.y) / MAP_WIDTH + .5,
+          img_path = "images/icons/tin.png",
+          img_offset=Position(-0.02538, -0.02625)),
+     Icon("door",
+          Position(0.3972, 0.627),
+          lambda pos: MAP_WIDTH * 0.17,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: -3 * (pos.x + pos.y * 4) / MAP_WIDTH + 1.5,
+          img_path = "images/icons/tin.png",
+          img_offset=Position(-0.02538, -0.02625)),
+     Icon("door",
+          Position(0.4546, 0.60175),
+          lambda pos: MAP_WIDTH * 0.2,# * (0.3 + 0.15 * cos(4.5 * pos.x / 100)),
+          lambda pos: 9 * (pos.x + pos.y) / MAP_WIDTH + 5.5,
+          img_path = "images/icons/tin.png",
+          img_offset=Position(-0.02538, -0.02625)),
 ]
